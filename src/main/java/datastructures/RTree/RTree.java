5ac4de6f593b80c466411d3de5e2dcd676ff5efd
Iscle <albertiscle9@gmail.com>
2019-04-28 16:48:11 +0200

    Start R-Tree insertion implementation

--- src/main/java/datastructures/RTree/RTree.java
+++ src/main/java/datastructures/RTree/RTree.java
@@ -1,6 +1,5 @@
 package datastructures.RTree;
 
-import models.Point;
 import models.Post;
 
 public class RTree {
@@ -10,20 +9,40 @@ public class RTree {
     private Node root;
 
     public RTree() {
-        root = new LeafNode();
+        root = new LeafNode(null);
     }
 
     public void addPost(Post post) {
-        // TODO: implement this function
+        Node nextNode = root;
+
+        while (nextNode instanceof InternalNode) {
+
+            Node[] child = ((InternalNode) nextNode).getChild();
+
+            for (Node n : child) {
+                if (n instanceof LeafNode) {
+
+                } else if (n instanceof InternalNode){
+
+                }
+            }
+        }
+
+        assert (nextNode instanceof LeafNode);
+        if (nextNode.isFull()) {
+
+        } else {
+            ((LeafNode) nextNode).addPost(post);
+        }
     }
 
-    // Returns the post on that specific point
-    public Post getPost(Point point) {
+    // Returns the post on that specific location
+    public Post getPost(double[] location) {
         return null; // TODO: implement this function
     }
 
     // Returns the posts inside that region
-    public Post[] getPosts(Point[] corners) {
+    public Post[] getPosts(double[] corners) {
         return null; // TODO: implement this function
     }
 }
