0de14a1d4cf012352e6bc1d41d3418a9a7fac8af
cristian-g <cristian@cristiangonzalez.com>
2019-04-29 12:55:46 +0200

    Update TreePrinter.java

--- src/main/java/utils/print/TreePrinter.java
+++ src/main/java/utils/print/TreePrinter.java
@@ -5,11 +5,15 @@ import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.Graph;
 import guru.nidi.graphviz.model.LinkTarget;
+import guru.nidi.graphviz.model.Node;
 
 import java.io.File;
 import java.io.IOException;
 
+import static guru.nidi.graphviz.attribute.Records.rec;
+import static guru.nidi.graphviz.model.Compass.*;
 import static guru.nidi.graphviz.model.Factory.*;
+import static guru.nidi.graphviz.model.Link.between;
 
 public class TreePrinter {
     public static int max_recursive = Integer.MAX_VALUE;// Limit can be "disabled" using Integer.MAX_VALUE
@@ -64,5 +68,36 @@ public class TreePrinter {
     public void resetStats() {
         max_recursive = Integer.MAX_VALUE;// Limit can be disabled using Integer.MAX_VALUE
         count = 0;
+    }
+
+    public void printRTreeExample() {
+
+        Node
+                node0 = node("node0").with(Records.of(rec("f0", ""), rec("f1", ""), rec("f2", ""), rec("f3", ""), rec("f4", ""))),
+                node1 = node("node1").with(Records.of(rec("n4"), rec("v", "719"), rec(""))),
+                node2 = node("node2").with(Records.of(rec("a1"), rec("805"), rec("p", ""))),
+                node3 = node("node3").with(Records.of(rec("i9"), rec("718"), rec(""))),
+                node4 = node("node4").with(Records.of(rec("e5"), rec("989"), rec("p", ""))),
+                node5 = node("node5").with(Records.of(rec("t2"), rec("v", "959"), rec(""))),
+                node6 = node("node6").with(Records.of(rec("o1"), rec("794"), rec(""))),
+                node7 = node("node7").with(Records.of(rec("s7"), rec("659"), rec("")));
+        Graph g = graph("example3").directed()
+                .graphAttr().with(RankDir.TOP_TO_BOTTOM)
+                .with(
+                        node0.link(
+                                between(port("f0"), node1.port("v", SOUTH)),
+                                between(port("f1"), node2.port(WEST)),
+                                between(port("f2"), node3.port(WEST)),
+                                between(port("f3"), node4.port(WEST)),
+                                between(port("f4"), node5.port("v", NORTH))),
+                        node2.link(between(port("p"), node6.port(NORTH_WEST))),
+                        node4.link(between(port("p"), node7.port(SOUTH_WEST))));
+
+        try {
+            Graphviz.fromGraph(g).width(900).render(Format.PNG).toFile(new File("out/r_tree_example.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
