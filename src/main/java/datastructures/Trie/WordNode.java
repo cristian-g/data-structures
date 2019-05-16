a45a621e927c746ffdfcce9add289e1cb656757f
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2019-05-16 15:07:56 +0200

    Added User pointer to Trie.

--- src/main/java/datastructures/Trie/WordNode.java
+++ src/main/java/datastructures/Trie/WordNode.java
@@ -1,27 +1,39 @@
 package datastructures.Trie;
 
+import models.User;
 import utils.print.PrintableNode;
 
 public class WordNode extends Node implements PrintableNode {
     private int searches;
+    private User user;
+
+    public WordNode(String word, User user) {
+        super(word);
+        this.searches = 0;
+        this.user = user;
+    }
+
+    public WordNode(Node n, User user) {
+        super(n.getWord(), n.getChilds());
+        this.searches = 0;
+        this.user = user;
+    }
 
     public WordNode(int searches) {
         super();
         this.searches = searches;
     }
 
-    public WordNode(String word) {
-        super(word);
-        this.searches = 0;
-    }
-
-    public WordNode(Node n) {
-        super(n.getWord(), n.getChilds());
-        this.searches = 0;
-    }
-
     public int getSearches() {
         return searches;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public void incrementSearches() {
