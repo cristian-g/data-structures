d6abaf39a742a0a6cd67156478beeba3ab7a42b4
FerranMontoliu <ferran.montoliu@students.salle.url.edu>
2019-05-17 12:22:07 +0200

    Started quadratic split R-Tree.

--- src/main/java/datastructures/RTree/RTree.java
+++ src/main/java/datastructures/RTree/RTree.java
@@ -1,6 +1,7 @@
 package datastructures.RTree;
 
 import datastructures.ElementWithCoordinates;
+import datastructures.LinkedList.LinkedList;
 import models.Post;
 
 public class RTree {
@@ -125,4 +126,89 @@ public class RTree {
 
         return false;
     }
+
+    public Node[] split(LeafNode n, Post p) {
+        Post[] postsArr = new Post[n.getPosts().length + 1];
+        for(int i = 0; i < n.getPosts().length; i++) {
+            postsArr[i] = n.getPosts()[i];
+        }
+        postsArr[n.getPosts().length] = p;
+
+        //Troba els 2 posts mes allunyats entre ells:
+        Post[] posts = findFurthestPosts(postsArr);
+
+        //Ara ja tenim els 2 posts mes allunyats entre ells.
+        //Creem 2 regions i 2 leafNodes i inserim un post a cada leafNode:
+        InternalNode region1 = new InternalNode(n.getParent());
+        InternalNode region2 = new InternalNode(n.getParent());
+        LeafNode leaf1 = new LeafNode(region1);
+        LeafNode leaf2 = new LeafNode(region2);
+        leaf1.addPost(posts[0]);
+        leaf2.addPost(posts[1]);
+
+        //Ara que ja tenim els 2 posts en les seves regions, s'han d'afegir la resta de posts entre la R1 i la R2:
+        addRemainingPosts(leaf1, leaf2, postsArr, posts);
+        Node[] res = new Node[4];
+        res[0] = region1;
+        res[1] = region2;
+        res[2] = leaf1;
+        res[3] = leaf2;
+        return res;
+    }
+
+    public void addRemainingPosts(LeafNode leaf1, LeafNode leaf2, Post[] postsArr, Post[] posts) {
+        int postsToAdd = postsArr.length - 2;
+        for(Post p: postsArr) {
+            if(p != posts[0] && p != posts[1]) {
+                boolean firstRegion = findNearestRegion(leaf1, leaf2, p);
+                if(postsToAdd > Math.abs(leaf1.getLength() - leaf2.getLength())) {
+                    if(firstRegion) {
+                        leaf1.addPost(p);
+                    } else {
+                        leaf2.addPost(p);
+                    }
+                } else {
+                    if(leaf1.getLength() > leaf2.getLength()) {
+                        leaf2.addPost(p);
+                    } else {
+                        leaf1.addPost(p);
+                    }
+                }
+                postsToAdd--;
+            }
+        }
+    }
+
+    //Mira a quina de les dues regions hi hauria increment d'area mes petit per a inserir el post en aquella regio:
+    public boolean findNearestRegion(LeafNode leaf1, LeafNode leaf2, Post p) {
+        double inc1 = calculaIncrement(leaf1.getStart(), leaf1.getEnd(), p.getLocation());
+        double inc2 = calculaIncrement(leaf2.getStart(), leaf2.getEnd(), p.getLocation());
+        return inc1 > inc2;
+    }
+
+
+    //Retorna els dos posts mes allunyats entre ells:
+    public Post[] findFurthestPosts(Post[] postsArr) {
+        double max = 0;
+        Post[] posts = new Post[2];
+        for(int i = 0; i < postsArr.length; i++) {
+            for(int j = i + 1; j < postsArr.length; j++) {
+                double dist = calculateDistance(postsArr[i], postsArr[j]);
+                if(dist > max) {
+                    max = dist;
+                    posts[0] = postsArr[i];
+                    posts[1] = postsArr[j];
+                }
+            }
+        }
+        return posts;
+    }
+
+    //Retorna la distancia al quadrat que hi ha entre post1 i post2:
+    public double calculateDistance(Post post1, Post post2) {
+        double[] locationP1 = post1.getLocation();
+        double[] locationP2 = post2.getLocation();
+        return (Math.pow(locationP1[0] - locationP2[0], 2) + Math.pow(locationP1[1] - locationP2[1], 2));
+    }
+
 }
