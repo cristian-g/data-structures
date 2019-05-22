31c0232a160428338d0222c6b0a8aa3ffeda1287
cristian-g <cristian@cristiangonzalez.com>
2019-05-22 20:28:38 +0200

    Update HashTable.java

--- src/main/java/datastructures/HashTable/HashTable.java
+++ src/main/java/datastructures/HashTable/HashTable.java
@@ -5,7 +5,6 @@ import datastructures.ElementWithIntegerKey;
 import datastructures.ElementWithStringKey;
 import datastructures.LinkedList.LinkedList;
 
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 
 import static java.lang.Math.toIntExact;
@@ -14,8 +13,8 @@ public class HashTable<E> {
 
     public static String DATA_STRUCTURE_NAME = "Hash table";
 
-    //public static int DEFAULT_SIZE = 20749;
-    public static int DEFAULT_SIZE = 20;// Use this size just in case you want to export image of hashtable
+    public static int DEFAULT_SIZE = 20749;
+    //public static int DEFAULT_SIZE = 20;// Use this size just in case you want to export image of hashtable
     private LinkedList<E>[] array;
 
     public HashTable() {
